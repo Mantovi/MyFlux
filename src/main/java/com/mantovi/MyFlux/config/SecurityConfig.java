e3e55e696572fdbf82346231d971f90dedb09a96
Mantovi <leandromandrade01@gmail.com>
2026-03-28 15:25:13 -0300

    Segurança e Token

--- src/main/java/com/mantovi/MyFlux/config/SecurityConfig.java
+++ src/main/java/com/mantovi/MyFlux/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration //Define essa classe como uma classe de configuração.
@@ -12,12 +13,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http)  throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable) // = csrf -> csrf.disable() -- Desabilita o csrf
-                .authorizeHttpRequests(auth -> auth //define as regras de acessora para as rotas da api
-                        .requestMatchers("/users/**").permitAll() //libera rota como publica
-                        .anyRequest().authenticated()); //outras rotas, fora as que estão configuradas nesse arquivo, precisam de autenticação
-        return http.build(); //Finaliza a configuração e cria o objeto de segurança que o Spring vai usar.
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity)  throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
-}
+    }
+
+
