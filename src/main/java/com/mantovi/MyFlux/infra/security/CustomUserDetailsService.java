8675962a32f47070024e302e081ca4224e702aef
Mantovi <leandromandrade01@gmail.com>
2026-03-30 12:35:12 -0300

    Melhora na segurança

---
+++ src/main/java/com/mantovi/MyFlux/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.mantovi.MyFlux.infra.security;
+
+import com.mantovi.MyFlux.model.User;
+import com.mantovi.MyFlux.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService  implements UserDetailsService {
+
+    @Autowired
+    private UserRepository  userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
