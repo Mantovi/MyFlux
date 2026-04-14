bc95d5f128ec5cf304d8197579109959de59ed35
Mantovi <leandromandrade01@gmail.com>
2026-04-14 12:40:41 -0300

    Sem Id na resposta da conta

--- src/main/java/com/mantovi/MyFlux/dto/AccountResponseDTO.java
+++ src/main/java/com/mantovi/MyFlux/dto/AccountResponseDTO.java
@@ -4,10 +4,8 @@ import com.mantovi.MyFlux.model.AccountType;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.UUID;
 
 public record AccountResponseDTO(
-        UUID id,
         String name,
         AccountType accountType,
         BigDecimal openingBalance,
